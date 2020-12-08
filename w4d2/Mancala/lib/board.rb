c9846800b7d20d4416ff4211aafb4eb05bd47877
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-07 17:57:23 -0800

    Adds completed Mancala game

--- w4d2/Mancala/lib/board.rb
+++ w4d2/Mancala/lib/board.rb
@@ -2,20 +2,60 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) {Array.new}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    (0..5).each do |i|
+      @cups[i].concat([:stone]*4)
+      @cups[i+7].concat([:stone]*4)
+    end
   end
 
   def valid_move?(start_pos)
+    raise 'Invalid starting cup' unless start_pos.between?(0,13)
+    raise 'Starting cup is empty' if @cups[start_pos].empty?
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    idx = start_pos
+    hand = @cups[idx].length
+    @cups[idx] = []
+    
+    
+    # if current_player_name == @name1
+    #   opp_store = 13
+    # else
+    #   opp_store = 6
+    # end
+
+    opp_store = current_player_name == @name1 ? 13 : 6
+
+    while hand > 0
+      idx = (idx+1)%14
+      if idx != opp_store
+        @cups[idx].concat([:stone])
+        hand -= 1
+      end
+    end
+    render
+    next_turn(idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -27,8 +67,17 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0..5].all?(&:empty?) || @cups[7..12].all?(&:empty?)
   end
 
   def winner
+    case @cups[6] <=> @cups[13]
+    when -1
+      @name2
+    when 0
+      :draw
+    when 1
+      @name1
+    end
   end
 end
